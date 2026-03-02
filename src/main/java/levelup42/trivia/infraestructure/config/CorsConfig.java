b2dd72a657ab77e08152fa7b0471231bfc4011d1
Aarón Gómez <nomeacuerdobien@gmail.com>
2026-03-02 21:24:06 +0100

    pruebas flutter 2

---
+++ src/main/java/levelup42/trivia/infraestructure/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package levelup42.trivia.infraestructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // En producción es mejor especificar los orígenes exactos
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
